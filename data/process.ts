75c7a851c8058d9c86a63b2de81fe896b855b624
Olivier Chabal <olivierchabal@mac.home>
2026-08-02 10:53:28 +0200

    Expand textile selection process story

--- data/process.ts
+++ data/process.ts
@@ -1,6 +1,6 @@
 export const process = [
-  ["01", "Traitement des déchets", "Des textiles industriels arrivent en fin de vie ou sont destinés à être éliminés."],
-  ["02", "Sélection", "Les textiles fortement chargés en polyester sont sélectionnés."],
+  ["01", "Traitement des déchets", "Chaque année, des milliers de tonnes de textiles industriels arrivent en fin de vie. Faute de solution de valorisation, une partie est encore incinérée, enfouie ou exportée. UN1QPEN offre une autre voie : donner une seconde vie à ces textiles en les transformant en une nouvelle matière première, sans utiliser de nouveau pétrole."],
+  ["02", "Sélection", "Les textiles fortement chargés en polyester sont sélectionnés dans l’usine de Lyon."],
   ["03", "Transformation", "La matière polyester est récupérée et transformée."],
   ["04", "Injection", "La nouvelle matière est utilisée pour injecter le corps du stylo."],
   ["05", "Nouvel usage", "Le stylo est assemblé, personnalisé et devient un objet média."],
