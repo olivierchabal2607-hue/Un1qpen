a67c9359050a36fa3aa25706739ff1b726b4b570
Olivier Chabal <olivierchabal@MacBook-Air-de-Olivier.local>
2026-08-20 10:44:58 +0200

    Add home navigation link

--- data/navigation.ts
+++ data/navigation.ts
@@ -1,4 +1,5 @@
 export const navigation = [
+  { label: "Accueil", href: "/" },
   { label: "L’histoire", href: "/histoire" },
   { label: "La matière", href: "/matiere" },
   { label: "Le produit", href: "/produit" },
